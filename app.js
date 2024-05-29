047cd51b3d0b2b16563365cbb07db59eb8cbf34a
Fulgrim <ivanyuschin.work@yandex.ru>
2024-05-29 14:50:23 +0300

    feat: develop base server for app

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ require("dotenv").config();
 const { PORT = 3000 } = process.env;
 
 app.listen(PORT, (err) =>
-  err ? console.log(err) : console.log(`server listen ${PORT} port`)
+  err ? console.log(err) : console.log(`server listen ${PORT} PORT`)
 );
