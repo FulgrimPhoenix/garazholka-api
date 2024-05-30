6989ffc7752d106ccb97e501cf313b2fa101ee38
Fulgrim <ivanyuschin.work@yandex.ru>
2024-05-30 15:13:35 +0300

    feat: develop user schema with self method

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 require("dotenv").config();
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, URL = "mongodb://0.0.0.0:27017/garazholkadb" } =
+  process.env;
+
+mongoose
+  .connect(URL)
+  .then(() => console.log("connected to database"))
+  .catch((err) => console.log(`connection failed: ${err}`));
 
 app.listen(PORT, (err) =>
   err ? console.log(err) : console.log(`server listen ${PORT} PORT`)
